e9a3897e694df1f49a824ccc8aef60142b03bc5b
Allan David <allanbrados@gmail.com>
2018-04-12 20:48:37 -0300

    Create API to generate single event

---
+++ Assets/Scripts/Telemetry/Telemetry.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Dynamic;
+using UnityEngine;
+
+public class Telemetry {
+
+	static public int createSingleEvent (string name, Vector3 position, ExpandoObject extraInfo = null) {
+		TelemetryNode playerDeath = new TelemetryNode (
+			TelemetryNodeType.SingleEvent,
+			name,
+			position,
+			extraInfo
+		);
+
+		return TelemetryCore.addNode (playerDeath);
+	}
+}
