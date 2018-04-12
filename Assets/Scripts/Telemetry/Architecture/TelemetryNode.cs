dce98bed306335c6c2fcc3b6fce862798d93d6dc
Allan David <allanbrados@gmail.com>
2018-04-12 20:41:34 -0300

    Changes the TelemetryNode contructor call

--- Assets/Scripts/Telemetry/Architecture/TelemetryNode.cs
+++ Assets/Scripts/Telemetry/Architecture/TelemetryNode.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using System.Runtime.Serialization;
 using System.Dynamic;
+using System.Runtime.Serialization;
 using Newtonsoft.Json;
 using UnityEngine;
 
@@ -14,48 +14,39 @@ public class TelemetryNode : ISerializable {
 	Vector3 position;
 	ExpandoObject info;
 
-	public TelemetryNode(TelemetryNodeType nodeType, string name, Vector3 position, ExpandoObject info) {
+	public TelemetryNode (TelemetryNodeType nodeType, string name, Vector3 position, ExpandoObject info = null) {
 		this.nodeType = nodeType;
 		this.link = -1;
 		this.name = name;
 		this.position = position;
-		this.info = info;
+
+		this.info = info != null ? info : new ExpandoObject ();
+
 		this.time = Time.realtimeSinceStartup;
 	}
 
-	public TelemetryNode(TelemetryNodeType nodeType, string name, Vector3 position)
-    {
-			this.nodeType = nodeType;
-			this.link = -1;
-			this.name = name;
-			this.position = position;
-			this.time = Time.realtimeSinceStartup;
-			this.info = new ExpandoObject();
-    }
+	public TelemetryNodeType getType () { return this.nodeType; }
+	public string getName () { return this.name; }
+	public float getTime () { return this.time; }
+	public Vector3 getPosition () { return this.position; }
+	public ExpandoObject getInfo () { return this.info; }
 
-	public TelemetryNodeType getType() { return this.nodeType; }
-	public string getName() { return this.name; }
-	public float getTime() { return this.time; }
-	public Vector3 getPosition() { return this.position; }
-	public ExpandoObject getInfo() { return this.info; }
-
-	public void setLink(int nodeId) { 
+	public void setLink (int nodeId) {
 		this.link = nodeId;
 	}
 
-	public void GetObjectData(SerializationInfo info, StreamingContext context)
-	{
-		info.AddValue("Type", this.nodeType.Value, typeof(string));
-		info.AddValue("Link", this.link, typeof(int));
-		info.AddValue("Name", this.name, typeof(string));
-		info.AddValue("Time", this.time, typeof(float));
-		
-		dynamic position = new ExpandoObject();
-			position.x = this.position.x;
-			position.y = this.position.y;
-			position.z = this.position.z;
-		info.AddValue("Position", position, typeof(ExpandoObject));
+	public void GetObjectData (SerializationInfo info, StreamingContext context) {
+		info.AddValue ("Type", this.nodeType.Value, typeof (string));
+		info.AddValue ("Link", this.link, typeof (int));
+		info.AddValue ("Name", this.name, typeof (string));
+		info.AddValue ("Time", this.time, typeof (float));
 
-		info.AddValue("Info", this.info, typeof(ExpandoObject));		
+		dynamic position = new ExpandoObject ();
+		position.x = this.position.x;
+		position.y = this.position.y;
+		position.z = this.position.z;
+		info.AddValue ("Position", position, typeof (ExpandoObject));
+
+		info.AddValue ("Info", this.info, typeof (ExpandoObject));
 	}
 }
