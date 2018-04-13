812ec3e90bfbb3879bdb1e63ab942f9ce0c13000
Allan David <allanbrados@gmail.com>
2018-04-12 21:46:29 -0300

    Creates API to generate chain event

--- Assets/Scripts/Rocket.cs
+++ Assets/Scripts/Rocket.cs
@@ -1,68 +1,48 @@
-﻿using UnityEngine;
-using System.Collections;
+﻿using System.Collections;
+using UnityEngine;
 
-public class Rocket : MonoBehaviour 
-{
-	public GameObject explosion;		// Prefab of explosion effect.
+public class Rocket : MonoBehaviour {
+	public GameObject explosion; // Prefab of explosion effect.
 	int shotEventNodeId = -1;
 
-	void Start () 
-	{
+	void Start () {
 		// Telemetry: Create a Chain Event Node
-		TelemetryNode playerShoot = new TelemetryNode(
-			TelemetryNodeType.ChainEvent,
-			"Player shot",
-			transform.position
-		);
-
-		// Telemetry: Save the event node ID
-		shotEventNodeId = TelemetryCore.addNode(playerShoot);
+		shotEventNodeId = Telemetry.createChainEvent ("Player shot", transform.position);
 
 		// Destroy the rocket after 2 seconds if it doesn't get destroyed before then.
-		Destroy(gameObject, 2);
+		Destroy (gameObject, 2);
 	}
 
-
-	void OnExplode()
-	{
+	void OnExplode () {
 		// Create a quaternion with a random rotation in the z-axis.
-		Quaternion randomRotation = Quaternion.Euler(0f, 0f, Random.Range(0f, 360f));
+		Quaternion randomRotation = Quaternion.Euler (0f, 0f, Random.Range (0f, 360f));
 
 		// Instantiate the explosion where the rocket is with the random rotation.
-		Instantiate(explosion, transform.position, randomRotation);
+		Instantiate (explosion, transform.position, randomRotation);
 	}
-	
-	void OnTriggerEnter2D (Collider2D col) 
-	{
+
+	void OnTriggerEnter2D (Collider2D col) {
 		// If it hits an enemy...
-		if(col.tag == "Enemy")
-		{
+		if (col.tag == "Enemy") {
 			// ... find the Enemy script and call the Hurt function.
-			col.gameObject.GetComponent<Enemy>().Hurt();
+			col.gameObject.GetComponent<Enemy> ().Hurt ();
 
-            // Telemetry: Creates new event to be linked with the previous one
-            // This generates a relation of cause and consequence between them
-            TelemetryNode enemyHit = new TelemetryNode(
-				TelemetryNodeType.ChainEvent,
-				"Enemy was hit",
-				col.transform.position
-			);
-
-            // Telemetry: Links the new event with the previous one (Player Shot)
-            enemyHit.setLink(shotEventNodeId);
-            TelemetryCore.addNode(enemyHit);
+			// Telemetry: Creates new event to be linked with the previous one
+			// This generates a relation of cause and consequence between them
+			Telemetry.createChainEvent ("Enemy was hit", transform.position, shotEventNodeId);
 
 			// Call the explosion instantiation.
-			OnExplode();
+			OnExplode ();
 
 			// Destroy the rocket.
 			Destroy (gameObject);
 		}
 		// Otherwise if it hits a bomb crate...
-		else if(col.tag == "BombPickup")
-		{
+		else if (col.tag == "BombPickup") {
 			// ... find the Bomb script and call the Explode function.
-			col.gameObject.GetComponent<Bomb>().Explode();
+			col.gameObject.GetComponent<Bomb> ().Explode ();
+
+			Telemetry.createChainEvent ("BombPickup was hit", transform.position, shotEventNodeId);
 
 			// Destroy the bomb crate.
 			Destroy (col.transform.root.gameObject);
@@ -71,10 +51,11 @@ public class Rocket : MonoBehaviour
 			Destroy (gameObject);
 		}
 		// Otherwise if the player manages to shoot himself...
-		else if(col.gameObject.tag != "Player")
-		{
+		else if (col.gameObject.tag != "Player") {
+			Telemetry.createChainEvent ("Something was hit", transform.position, shotEventNodeId);
+
 			// Instantiate the explosion and destroy the rocket.
-			OnExplode();
+			OnExplode ();
 			Destroy (gameObject);
 		}
 	}
--- Assets/Scripts/Telemetry/Telemetry.cs
+++ Assets/Scripts/Telemetry/Telemetry.cs
@@ -6,13 +6,28 @@ using UnityEngine;
 public class Telemetry {
 
 	static public int createSingleEvent (string name, Vector3 position, ExpandoObject extraInfo = null) {
-		TelemetryNode playerDeath = new TelemetryNode (
+		TelemetryNode newSingleEvent = new TelemetryNode (
 			TelemetryNodeType.SingleEvent,
 			name,
 			position,
 			extraInfo
 		);
 
-		return TelemetryCore.addNode (playerDeath);
+		return TelemetryCore.addNode (newSingleEvent);
+	}
+
+	static public int createChainEvent (string name, Vector3 position, int previousEventId = -1, ExpandoObject extraInfo = null) {
+		TelemetryNode newChainEvent = new TelemetryNode (
+			TelemetryNodeType.ChainEvent,
+			name,
+			position,
+			extraInfo
+		);
+
+		if(previousEventId != -1)
+			newChainEvent.setLink (previousEventId);
+
+		// Telemetry: Save the event node ID
+		return TelemetryCore.addNode (newChainEvent);
 	}
 }
